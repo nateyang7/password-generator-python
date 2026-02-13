12d0123ad9e5b1dd619faa35278e69c746ffb682
Nathan Yang <nathanyang.w@outlook.com>
2026-02-13 16:23:29 +0100

    Add function is_strong to test a password

---
+++ README.md
@@ -0,0 +1,26 @@
+# Password Generator
+
+## Introduction
+**password-generator-python** is a utility tool to generate a strong password. 
+This project serves as an educative practice for cybersecurity purpose.
+
+## Table of contents
+- [Features](##Features)
+- [Requirements](##Requirements)
+- [Installation](##Installation)
+- [Usage](##Usage)
+
+## Features
+- [] Generate a strong password
+
+## Requirements
+None
+
+## Installation
+Clone the repository:
+```bash
+git clone https://github.com/NateYang7/password-generator-python.git
+```
+
+## Usage
+### How to launch it ?
---
+++ src/password_generator.py
@@ -0,0 +1,44 @@
+def is_strong(password: str) -> bool:
+    """
+    Tests if a password is strong.
+
+    Args:
+        password (str): Password to test.
+
+    Returns:
+        bool: True if password is strong else False.
+
+    Examples:
+        >>> is_strong("Qwerty41!")
+        True
+        >>> is_strong("1234")
+        False
+
+    Notes:
+        A password is strong if it validates this requirements:
+        - >= 8 characters
+        - At least 1 capital letter
+        - At least 1 small letter
+        - At least 1 digit
+        - At least 1 special character (!@#$%&*)
+    """
+    requirements: dict[str, bool] = {
+        "has1cap": False,
+        "has1small": False,
+        "has1digit": False,
+        "has1spec": False,
+    }
+    special_characters: str = "!@#$%&*"
+
+    for char in password:
+        if char.isupper():
+            requirements["has1cap"] = True
+        if char.islower():
+            requirements["has1small"] = True
+        if char.isdigit():
+            requirements["has1digit"] = True
+        if char in special_characters:
+            requirements["has1spec"] = True
+
+    return all(requirements.values())
+
